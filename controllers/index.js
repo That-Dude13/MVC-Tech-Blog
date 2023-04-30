69a90deda1aa9a96871eaa3b4f864a6d19adf3c4
Change Agent <keasiley@icloud.com>
2023-04-30 17:48:28 -0500

    index file edited.

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
-const homeRoutes = require('./homeRoutes');
+const postRoutes = require('./postRoutes');
 
-router.use('/', homeRoutes);
+router.use('/', postRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
