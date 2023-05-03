31fce713e2067256aca810d08f5c16b8ee95e66e
Change Agent <keasiley@icloud.com>
2023-05-03 14:56:42 -0500

    changes made to dashboard, main, login, and signup handle bars. signup.js has been created, login.js has been edited.

--- public/js/login.js
+++ public/js/login.js
@@ -22,37 +22,3 @@ const loginFormHandler = async (event) => {
   }
 };
 
-const signupFormHandler = async (event) => {
-  event.preventDefault();
-
-  const name = document.querySelector('.name-signup').value;
-  const email = document.querySelector('.email-signup').value;
-  const password = document.querySelector('.password-signup').value;
-
-  if (name && email && password) {
-    const response = await fetch('/api/users', {
-      method: 'POST',
-      body: JSON.stringify({ name, email, password }),
-      headers: { 'Content-Type': 'application/json' },
-    });
-
-    if (response.ok) {
-      document.location.replace('/profile');
-    } else {
-      alert(response.statusText);
-    }
-  }
-};
-if (document.location.pathname === "/login"){
-  document
-  .querySelector('.login-form')
-  .addEventListener('submit', loginFormHandler);
-}else if (document.location.pathname ==="/signup"){
-  document
-  .querySelector('.signup-form')
-  .addEventListener('submit', signupFormHandler);
-
-}
-
-
-
